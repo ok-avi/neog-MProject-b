c93b1e15709b01a434847e03061e9d2099113f9b
ok-avi <avi11574kumar@gmail.com>
2025-06-14 15:24:40 +0530

    Chore: refactor children routes; rearranged single layout for all page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,172 +1,21 @@
-import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
-import useFetch from "./useFetch";
-import { useState } from "react";
-import Loading from "./components/Loading";
+
+import Dashboard from "./pages/Dashboard";
 
 const App = () => {
-  const [filter, setFilter] = useState();
-  const { data, loading, error } = useFetch(
-    "https://neog-m-project-b-backend.vercel.app/api/v1/leads"
-  );
-  const leadArray = ["Lead 1", "Lead 2", "Lead 3"];
-  const filteredLead = filter
-    ? data.filter((lead) => lead.status === filter)
-    : data;
-  // console.log(data && data);
   return (
-    <div style={{minHeight:""}}>
-      <header className="border-bottom border-3 text-center py-3">
-        <h1>Anvaya CRM Dashboard</h1>
-      </header>
-      <main className="row ">
+      <div className="row " style={{height:"100vh",width:"100vw"}}>
         <section
-          style={{ height: "" }}
-          className=" col-4 col-lg-3    border-end  border-3 d-flex   "
+          style={{height:"100%"}}
+          className=" col-4 col-lg-3   border-end  border d-flex  h-100 "
         >
-          <Sidebar />
+            <Sidebar />
         </section>
-        <section className="col-8 col-lg-9">
-          {loading && <Loading />}
-          {data && data.length > 0 && (
-            <>
-              <section className="py-3 px-4">
-                <div className="mt-3 row  justify-content-md-between">
-                  <h2 className="col-md-4 ">Quick Filters </h2>
-                  <section className="col-md-4 mb-3  px-md-4 d-flex justify-content-md-end">
-                    <Link
-                      to="/lead/form"
-                      className="btn btn-outline-danger px-4"
-                    >
-                      Create Lead
-                    </Link>
-                  </section>
-                </div>
-                <div className="d-flex  flex-wrap  py-2">
-                  <div className="me-3 mb-3">
-                    <button
-                      onClick={(e) => setFilter(e.target.value)}
-                      value="New"
-                      className="btn btn-outline-secondary px-4"
-                    >
-                      New
-                    </button>
-                  </div>
-                  <div className="me-3  mb-3">
-                    <button
-                      onClick={(e) => setFilter(e.target.value)}
-                      value="Contacted"
-                      className="btn btn-outline-secondary px-4"
-                    >
-                      Contacted
-                    </button>
-                  </div>
-                  <div className="me-3 mb-3">
-                    <button
-                      onClick={(e) => setFilter(e.target.value)}
-                      value="Qualified"
-                      className="btn btn-outline-secondary px-4"
-                    >
-                      Qualified
-                    </button>
-                  </div>
-                  <div className="me-3  mb-3">
-                    <button
-                      onClick={(e) => setFilter(e.target.value)}
-                      value="Proposal Sent"
-                      className="btn btn-outline-secondary px-4"
-                    >
-                      Proposal Sent
-                    </button>
-                  </div>
-                </div>
-              </section>
-              <section className="d-flex justify-content-center flex-wrap  pb-3">
-                {/* {leadArray.map((lead) => (
-              <div className="border border-2 mx-2 mb-2 px-3 py-1">{lead}</div>
-            ))} */}
-                {filteredLead.map((lead) => (
-                  <div className="border border-2 mx-2 mb-3 px-3 py-1">
-                    <Link to={`/lead/${lead._id}`} className="nav-link">
-                      {lead.name}
-                    </Link>
-                  </div>
-                ))}
-              </section>
-              <section className="py-3 px-4">
-                <h2>Lead Status</h2>
-
-                <div className="row">
-                  <div className="col-lg-4 col-sm-6 mb-3">
-                    <div className="card">
-                      <div className="card-body">
-                        <h5 className="card-title">New</h5>
-                        <div className="display-3 fw-normal">
-                          {data.filter((lead) => lead.status === "New").length}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-lg-4 col-sm-6 mb-3">
-                    <div className="card">
-                      <div className="card-body">
-                        <h5 className="card-title">Contacted</h5>
-                        <div className="display-3 fw-normal">
-                          {
-                            data.filter((lead) => lead.status === "Contacted")
-                              .length
-                          }
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-lg-4 col-sm-6 mb-3">
-                    <div className="card">
-                      <div className="card-body">
-                        <h5 className="card-title">Qualified</h5>
-                        <div className="display-3 fw-normal">
-                          {
-                            data.filter((lead) => lead.status === "Qualified")
-                              .length
-                          }
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-lg-4 col-sm-6 mb-3">
-                    <div className="card">
-                      <div className="card-body">
-                        <h5 className="card-title">Proposal Sent</h5>
-                        <div className="display-3 fw-normal">
-                          {
-                            data.filter(
-                              (lead) => lead.status === "Proposal Sent"
-                            ).length
-                          }
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-lg-4 col-sm-6 mb-3">
-                    <div className="card">
-                      <div className="card-body">
-                        <h5 className="card-title">Closed</h5>
-                        <div className="display-3 fw-normal">
-                          {
-                            data.filter((lead) => lead.status === "Closed")
-                              .length
-                          }
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </section>
-            </>
-          )}
+        <section className="col-8 col-lg-9 p-0  h-100" style={{overflowY:"scroll"}}>
+            <Outlet />
         </section>
-      </main>
-    </div>
+      </div>
   );
 };
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,67 +1,73 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import App from './App.jsx'
-import LeadList from './pages/LeadList.jsx'
-import LeadDetails from './pages/LeadDetails.jsx'
+import App from "./App.jsx";
+import LeadList from "./pages/LeadList.jsx";
+import LeadDetails from "./pages/LeadDetails.jsx";
 
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.bundle.min.js"
-import {createBrowserRouter,RouterProvider} from "react-router-dom"
-import LeadForm from './pages/LeadForm.jsx'
-import SalesAgentList from './pages/SalesAgentList.jsx'
-import AgentForm from './pages/AgentForm.jsx'
-import SalesAgent from './pages/SalesAgent.jsx'
-import Status from './pages/Status.jsx'
-import Reports from './pages/Reports.jsx'
-import Setting from './pages/Setting.jsx'
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LeadForm from "./pages/LeadForm.jsx";
+import SalesAgentList from "./pages/SalesAgentList.jsx";
+import AgentForm from "./pages/AgentForm.jsx";
+import SalesAgent from "./pages/SalesAgent.jsx";
+import Status from "./pages/Status.jsx";
+import Reports from "./pages/Reports.jsx";
+import Setting from "./pages/Setting.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
 
 const router = createBrowserRouter([
   {
-    path:"/",
-    element:<App />
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Dashboard />,
+      },
+      {
+        path: "/leads",
+        element: <LeadList />,
+      },
+      {
+        path: "/lead/:id",
+        element: <LeadDetails />,
+      },
+      {
+        path: "/lead/form",
+        element: <LeadForm />,
+      },
+      {
+        path: "/sales-agent",
+        element: <SalesAgentList />,
+      },
+      {
+        path: "/sales-agent/form",
+        element: <AgentForm />,
+      },
+      {
+        path: "/sales-agent/:id",
+        element: <SalesAgent />,
+      },
+      {
+        path: "/status",
+        element: <Status />,
+      },
+      {
+        path: "/reports",
+        element: <Reports />,
+      },
+      {
+        path: "/settings",
+        element: <Setting />,
+      },
+    ],
   },
-  {
-    path:"/leads",
-    element:<LeadList />
-  },
-  {
-    path:"/lead/:id",
-    element:<LeadDetails />,
-  },
-  {
-    path:"/lead/form",
-    element:<LeadForm />
-  },
-  {
-    path:"/sales-agent",
-    element:<SalesAgentList />
-  },
-  {
-    path:"/sales-agent/form",
-    element:<AgentForm />
-  },
-  {
-    path:"/sales-agent/:id",
-    element:<SalesAgent />
-  },
-  {
-    path:"/status",
-    element:<Status />
-  },
-  {
-    path:"/reports",
-    element:<Reports />
-  },
-  {
-    path:"/settings",
-    element:<Setting />
-  }
-])
+]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
